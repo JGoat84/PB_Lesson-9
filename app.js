d23f40edd7a68b2d653c2447ac8c94125dab0b4d
Julia Goatly <juliagoatly@hotmail.com>
2021-11-23 19:53:58 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+// let varname = 'Julia'
+// let varage = 100
+
+
+
+// console.log('hello my name is ' + varname + ', and i am ' + varage + 'years old');
+
+// let varNumbero=1
+// console.log(varNumbero)
+
+let varboo = false
+let varloo = true
+
+let varText = 'hello everybody';
+console.log(varText.length)
+typeof varboo
